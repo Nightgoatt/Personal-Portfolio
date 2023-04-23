2c481074e63c84d4262c43676c469333b6cd01d8
nightgoatt <la_mendez15@outlook.com>
2023-04-22 23:18:37 -0600

    Update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Reveal effect
+
 function reveal() {
   var rev = document.querySelectorAll('.rev');
 
@@ -14,3 +16,38 @@ function reveal() {
 }
 
 window.addEventListener('scroll', reveal);
+
+//Slideshow
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('slide_show');
+  let dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
